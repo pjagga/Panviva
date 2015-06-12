168a57e8aabb17d97dc2f557efc69f95c4fbb0b2
Paul Mateos <pmateos@panviva.com>
2015-06-12 12:03:39 +1000

    Updated with new folders

--- UnitTestProject1/Properties/AssemblyInfo.cs
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTestProject1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SP_Automation")]
+[assembly: AssemblyDescription("SupportPoint test automation project")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCompany("Panviva")]
+[assembly: AssemblyProduct("SP_Automation")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
