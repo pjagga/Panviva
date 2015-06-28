35caf6ca948765d0fe19add9246238623f0d43e6
pjagga <pjagga89@gmail.com>
2015-06-29 09:39:15 +1000

    Added New PAge Document Management

---
+++ SP_Automation/PageModels/DocumentPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SP_Automation.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_Automation.PageModels
+{
+    class DocumentPage : BasePage
+    {
+        IWebDriver d;
+        public DocumentPage(IWebDriver driver)
+            : base(driver)
+        {
+            this.d = driver;
+            //Wait for title to be displayed 
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(Properties.Settings.Default.WaitTime));
+            wait.Until((d) => { return d.Title.Contains("Document : SupportPoint"); }); 
+        }
+
+        public void setName(string value)
+        {
+            WebDriverWait wait = new WebDriverWait(d, TimeSpan.FromSeconds(Properties.Settings.Default.WaitTime));
+            wait.Until(drv => drv.FindElement(By.XPath("//input[@type='text'][@name='name']")));
+            UICommon.SetValue(By.XPath("//input[@type='text'][@name='name']"), value, d);
+        }
+    }
+}
